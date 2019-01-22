277b29e798dfb7611fb13b7eb9f1ed36477af287
johnylab <joaobatistacomunic@hotmail.com>
2019-01-22 10:04:05 -0200

    Configurações de build atualizadas

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var cp = require('child_process');
-var rollup = require('gulp-rollup');
-var babel = require('gulp-babel');
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-
 var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -35,33 +31,35 @@ gulp.task('post-css', function () {
         .pipe(gulp.dest('_site/css/'));
 });
 
-gulp.task('bundle', function () {
-    gulp.src('./_scripts/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(rollup({
-            input: './_scripts/main.js',
-            format: 'iife'
-        }))
-        .on('error', console.log)
-        .pipe(babel({
-            "presets": ["es2015", "minify"]
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./js'));
-});
+// gulp.task('bundle', function () {
+//     gulp.src('./_scripts/**/*.js')
+//         .pipe(sourcemaps.init())
+//         .pipe(rollup({
+//             input: './_scripts/main.js',
+//             output: {
+//                 format: 'iife'
+//             }
+//         }))
+//         .on('error', console.log)
+//         .pipe(babel({
+//             "presets": ["es2015", "minify"]
+//         }))
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest('./js'));
+// });
 
-gulp.task('jekyll-rebuild', ['bundle', 'jekyll-build'], function () {
+gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function () {
     browserSync.reload();
-});
+}));
 
-gulp.task('browser-sync', ['bundle', 'jekyll-build'], function () {
+gulp.task('browser-sync', gulp.series('jekyll-build', function () {
     browserSync({
         server: {
             baseDir: '_site',
             routes: browserSyncRoutes
         }
     });
-});
+}));
 
 gulp.task('watch', function () {
     gulp.watch([
@@ -71,7 +69,6 @@ gulp.task('watch', function () {
         '_sections/**/*',
         '_sass/**/*',
         'css/**/*',
-        '_scripts/**/*',
         'js/**/*'
     ], ['jekyll-rebuild']);
     gulp.watch([
@@ -79,4 +76,4 @@ gulp.task('watch', function () {
     ], ['post-css']);
 });
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', gulp.series('browser-sync', 'watch'));
